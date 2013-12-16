1746b421c0ba81ff34beb2723001d0fd9d416ded
Aru Sahni <arusahni@gmail.com>
2013-12-15 21:47:23 -0500

    Added licensing blurbs

--- timecube.js
+++ timecube.js
@@ -1,12 +1,24 @@
+/* ===========================================================
+ * Timecube.js v0.5.0 (https://github.com/arusahni/timecube-js/)
+ *
+ * Copyright (c) 2013 Aru Sahni 
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ===========================================================
+ */
+
 (function() {
 	function Timecube() {
 		var colors = {
 			"common": [
-					"#000075" //blue
-					,"#0C0" //green
-					,"#F00" //red
-					,"#C00" //dark red
-					,"#000" //black
+				"#000075" //blue
+				,"#0C0" //green
+				,"#F00" //red
+				,"#C00" //dark red
+				,"#000" //black
 			],
 			"uncommon": [
 				"#33F" //light blue
